20bca41f8f2ffc340228487daf1850614041e790
EnzeXu <xue20@wfu.edu>
2022-04-23 20:50:17 -0400

    draw_2D is ready

---
+++ utils.py
@@ -0,0 +1,98 @@
+import matplotlib.pyplot as plt
+
+
+def draw_two_dimension(
+        y_lists,
+        x_list,
+        color_list,
+        line_style_list,
+        legend_list=None,
+        legend_fontsize=15,
+        fig_title=None,
+        fig_x_label="time",
+        fig_y_label="val",
+        show_flag=True,
+        save_flag=False,
+        save_path=None,
+        save_dpi=300,
+        fig_title_size=20,
+        fig_grid=False,
+        marker_size=0,
+        line_width=2,
+        x_label_size=15,
+        y_label_size=15,
+        number_label_size=15,
+        fig_size=(8, 6)
+) -> None:
+    """
+    Draw a 2D plot of several lines
+    :param y_lists: (list[list]) y value of lines, each list in which is one line. e.g., [[2,3,4,5], [2,1,0,-1], [1,4,9,16]]
+    :param x_list: (list) x value shared by all lines. e.g., [1,2,3,4]
+    :param color_list: (list) color of each line. e.g., ["red", "blue", "green"]
+    :param line_style_list: (list) line style of each line. e.g., ["solid", "dotted", "dashed"]
+    :param legend_list: (list) legend of each line, which CAN BE LESS THAN NUMBER of LINES. e.g., ["red line", "blue line", "green line"]
+    :param legend_fontsize: (float) legend fontsize. e.g., 15
+    :param fig_title: (string) title of the figure. e.g., "Anonymous"
+    :param fig_x_label: (string) x label of the figure. e.g., "time"
+    :param fig_y_label: (string) y label of the figure. e.g., "val"
+    :param show_flag: (boolean) whether you want to show the figure. e.g., True
+    :param save_flag: (boolean) whether you want to save the figure. e.g., False
+    :param save_path: (string) If you want to save the figure, give the save path. e.g., "./test.png"
+    :param save_dpi: (integer) If you want to save the figure, give the save dpi. e.g., 300
+    :param fig_title_size: (float) figure title size. e.g., 20
+    :param fig_grid: (boolean) whether you want to display the grid. e.g., True
+    :param marker_size: (float) marker size. e.g., 0
+    :param line_width: (float) line width. e.g., 1
+    :param x_label_size: (float) x label size. e.g., 15
+    :param y_label_size: (float) y label size. e.g., 15
+    :param number_label_size: (float) number label size. e.g., 15
+    :param fig_size: (tuple) figure size. e.g., (8, 6)
+    :return:
+    """
+    assert len(y_lists[0]) == len(x_list), "Dimension of y should be same to that of x"
+    assert len(y_lists) == len(line_style_list) == len(color_list), "number of lines should be fixed"
+    y_count = len(y_lists)
+    plt.figure(figsize=fig_size)
+    for i in range(y_count):
+        plt.plot(x_list, y_lists[i], markersize=marker_size, linewidth=line_width, c=color_list[i], linestyle=line_style_list[i])
+    plt.xlabel(fig_x_label, fontsize=x_label_size)
+    plt.ylabel(fig_y_label, fontsize=y_label_size)
+    plt.tick_params(labelsize=number_label_size)
+    if legend_list:
+        plt.legend(legend_list, fontsize=legend_fontsize)
+    if fig_title:
+        plt.title(fig_title, fontsize=fig_title_size)
+    if fig_grid:
+        plt.grid(True)
+    if save_flag:
+        plt.savefig(save_path, dpi=save_dpi)
+    if show_flag:
+        plt.show()
+    plt.clf()
+
+
+if __name__ == "__main__":
+    x_list = range(10000)
+    y_lists = [
+        [0.005 * i + 10 for i in x_list],
+        [-0.005 * i - 30 for i in x_list],
+        [0.008 * i - 10 for i in x_list],
+        [-0.006 * i - 20 for i in x_list],
+        [-0.001 * i - 5 for i in x_list]
+    ]
+    color_list = ["red", "blue", "green", "cyan", "black"]
+    line_style_list = ["dashed", "dotted", "dashdot", "dashdot", "dashdot"]
+    legend_list = ["red line", "blue line", "green line", "cyan line", "black line"]
+
+    draw_two_dimension(
+        y_lists=y_lists,
+        x_list=x_list,
+        color_list=color_list,
+        legend_list=legend_list,
+        line_style_list=line_style_list,
+        fig_title="Anonymous",
+        fig_size=(8, 6),
+        show_flag=True,
+        save_flag=False,
+        save_path=None
+    )
